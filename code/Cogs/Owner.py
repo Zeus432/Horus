786549af81d9fb555889ce9c60b1bc8916b1bcb7
Zeus432 <msiddhu@outlook.com>
2021-08-17 18:20:35 +0530

    Replace eval back

--- code/Cogs/Owner.py
+++ code/Cogs/Owner.py
@@ -5,6 +5,8 @@ from jishaku.codeblocks import codeblock_converter
 from Useful.Useful import *
 from Useful.settings import *
 import io
+from contextlib import redirect_stdout
+import textwrap
 # to expose to the eval command
 
 
@@ -41,11 +43,52 @@ class Owner(commands.Cog, command_attrs=dict(hidden=True)):
             return f'```py\n{e.__class__.__name__}: {e}\n```'
         return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
 
-    @commands.command(pass_context=True, name='eval', aliases = ['e','run'])
-    async def _eval(self, ctx, *, code: codeblock_converter):
-        """Evaluates code"""
-        jsk = self.bot.get_command("jishaku py")
-        await jsk(ctx,argument=code)
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates a code"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
     @_eval.error
     async def _eval_error(self, ctx, error, test = None):
