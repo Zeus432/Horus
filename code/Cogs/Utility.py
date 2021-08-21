bf14adcba0eb83243af4dcb51cd98e492c9dd260
Zeus432 <msiddhu@outlook.com>
2021-08-21 09:59:02 +0530

    add av and ~~steal~~ charinfo

--- code/Cogs/Utility.py
+++ code/Cogs/Utility.py
@@ -4,7 +4,7 @@ import discord
 from Useful.settings import *
 import datetime
 from dateutil.relativedelta import relativedelta
-import asyncio
+import unicodedata
 
 
 class Utility(commands.Cog):
@@ -61,7 +61,7 @@ class Utility(commands.Cog):
     async def userinfo(self, ctx, member: discord.Member = None):
         if type(member) != discord.Member:
             member = ctx.author
-        uiembed = discord.Embed(title=f"{member.display_name}・{member}",colour=member.color,timestamp=ctx.message.created_at, description=f"**User ID:** `{member.id}`")
+        uiembed = discord.Embed(title=f"{member.display_name}・{member}",colour=member.colour if member.colour != discord.Colour(000000) else self.bot.colour,timestamp=ctx.message.created_at, description=f"**User ID:** `{member.id}`")
         uiembed.set_thumbnail(url=member.avatar)
         uiembed.set_footer(text=f"{ctx.guild}", icon_url=ctx.guild.icon)
         uiembed.add_field(name="Joined Discord:", value= f"<t:{round(member.created_at.timestamp())}:D>\n(<t:{round(member.created_at.timestamp())}:R>)")
@@ -112,6 +112,24 @@ class Utility(commands.Cog):
             uiembed.add_field(name="Special Badges:", value=badge)
         uiembed.add_field(name="Servers:", value=f"{len([g.id for g in self.bot.guilds if g.get_member(member.id)])} shared")
         await ctx.send(embed=uiembed)
+    
+    @commands.command(name = "avatar", help = "Get a user's avatar", brief = "Get User Avatar", aliases = ['av'])
+    @commands.guild_only()
+    async def avatar(self, ctx, user: discord.Member = None):
+        user = user or ctx.author
+        colour = user.colour if user.colour != discord.Colour(000000) else self.bot.colour
+        embed = discord.Embed(colour=colour,timestamp=ctx.message.created_at)
+        embed.set_footer(text=f"{ctx.guild}", icon_url=ctx.guild.icon)
+        avatar = user.avatar.with_static_format('png')
+        jpgavatar = user.avatar.with_static_format('jpg')
+        webpavatar = user.avatar.with_static_format('webp')
+        embed.set_image(url=avatar)
+        embed.title = f"Avatar for {user}"
+        view = discord.ui.View()
+        view.add_item(discord.ui.Button(style=discord.ButtonStyle.link, url=f"{avatar}", label=".png link"))
+        view.add_item(discord.ui.Button(style=discord.ButtonStyle.link, url=f"{jpgavatar}", label=".jpg link"))
+        view.add_item(discord.ui.Button(style=discord.ButtonStyle.link, url=f"{webpavatar}", label=".webp link"))
+        await ctx.send(embed=embed,view=view)
 
     @commands.command(name='uptime')
     @commands.guild_only()
@@ -119,6 +137,21 @@ class Utility(commands.Cog):
         """Gets the uptime of the bot"""
         uptime_string = self.get_uptime()
         await ctx.channel.send(f'Whorus has been up for {uptime_string}.\nSince <t:{round(self.bot.launch_ts)}>')
+    
+    @commands.command()
+    async def charinfo(self, ctx, *, characters: str):
+        """Shows you information about a number of characters.
+        Only up to 25 characters at a time.
+        """
+
+        def to_string(c):
+            digit = f'{ord(c):x}'
+            name = unicodedata.name(c, 'Name not found.')
+            return f'`\\U{digit:>08}`: {name} - {c} \N{EM DASH} <http://www.fileformat.info/info/unicode/char/{digit}>'
+        msg = '\n'.join(map(to_string, characters))
+        if len(msg) > 2000:
+            return await ctx.send('Output too long to display.')
+        await ctx.send(msg)
 
 def setup(bot: commands.Bot):
     bot.add_cog(Utility(bot))
