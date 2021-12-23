4cb01f6775cb9f7dc19b836bcf26442daeefc3d1
Zeus432 <msiddhu@outlook.com>
2021-12-23 17:26:43 +0530

    test

--- Cogs/BotStuff/botstuff.py
+++ Cogs/BotStuff/botstuff.py
@@ -47,7 +47,7 @@ class BotStuff(commands.Cog):
         end = time.perf_counter()
         postgres_ping = (end - start) * 1000
 
-        embed = discord.Embed(description = f"```yaml\nTyping: {round(typing_ping, 1)} ms\nWebsocket: {round(self.bot.latency*1000)} ms\nDatabase: {round(postgres_ping, 1)} ms```", colour = discord.Colour(0x2F3136))
+        embed = discord.Embed(description = f"```yaml\nTypinggg: {round(typing_ping, 1)} ms\nWebsocket: {round(self.bot.latency*1000)} ms\nDatabase: {round(postgres_ping, 1)} ms```", colour = discord.Colour(0x2F3136))
 
         await msg.edit(content = "Pong \U0001f3d3", embed = embed)
 
