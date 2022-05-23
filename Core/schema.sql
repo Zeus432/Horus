4296af2ca8fa328d599518a26d0c27c0bc97b6fe
Zeus432 <msiddhu@outlook.com>
2022-05-23 10:08:08 +0530

    add schema.sql

---
+++ Core/schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS guilddata (
+    guildid BIGINT UNIQUE PRIMARY KEY,
+    prefix VARCHAR[] DEFAULT '{"h!"}',
+    blacklists jsonb DEFAULT '{"prevbl": 0, "blacklisted": false}',
+    server_bls jsonb DEFAULT '{}'
+);
+
+CREATE TABLE IF NOT EXISTS userdata (
+    userid BIGINT UNIQUE PRIMARY KEY,
+    blacklists jsonb DEFAULT '{"prevbl": 0, "blacklisted": false}'
+);
+
+CREATE TABLE IF NOT EXISTS todo (
+    userid BIGINT UNIQUE PRIMARY KEY,
+    lastupdated BIGINT NOT NULL,
+    data jsonb DEFAULT '{}'
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+    name VARCHAR NOT NULL,
+    serverid BIGINT NOT NULL,
+    content TEXT NOT NULL,
+    usage BIGINT DEFAULT 0,
+    info jsonb DEFAULT '{"owner":null, "created_at": null}',
+    aliases VARCHAR[] DEFAULT '{}'
+);
+
+CREATE TABLE IF NOT EXISTS buttonroles (
+    guildid BIGINT NOT NULL,
+    messageid BIGINT PRIMARY KEY,
+    channelid BIGINT NOT NULL,
+    role_emoji jsonb DEFAULT '{}',
+    config jsonb DEFAULT '{}'
+);
